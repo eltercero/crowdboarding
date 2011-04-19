720311b540aedf5a312c10d71f5721fc36ff1a46
Michael Koper <michaelkoper@gmail.com>
2011-04-19 23:01:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdboarding::Application.config.secret_token = '5e6eeff5292dca75a5ec7d9852b7e0cd68be165b9d68dfcae7cd84c5f133646ce821e262ba9c3857443d76825c29e809bfd9afd8c5db8ddd64fe71f57c440f12'
