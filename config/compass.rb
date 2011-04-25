fec08b66feb73ba4f9ae610703296b4974a691ff
Michael Koper <michaelkoper@gmail.com>
2011-04-25 19:55:42 +0200

    changed some heroku config compass stuff

--- config/compass.rb
+++ config/compass.rb
@@ -2,9 +2,11 @@
 # Require any additional compass plugins here.
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
+environment  = Compass::AppIntegration::Rails.env
+
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = RAILS_ENV == "production" ? 'tmp/stylesheets' : 'public/stylesheets'
+css_dir = RAILS_ENV == :production ? 'tmp/stylesheets' : 'public/stylesheets'
 sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
