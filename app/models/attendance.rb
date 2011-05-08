ab830fa7de336d7929548c248b6518a7f26d29c9
Michael Koper <michaelkoper@gmail.com>
2011-05-08 23:38:27 +0200

    added a validation for unique rows by user_id and related_user_id in relations, user_id and event_id in attendance.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,13 @@
-class Attendance < ActiveRecord::Base
+class Attendance < ActiveRecord::Base  
+  validates_presence_of :user_id, :event_id
   belongs_to :user
   belongs_to :event
   
+  validate :validate_unique_by_user_id_and_event_id
+  
+  private
+  
+    def validate_unique_by_user_id_and_event_id
+      self.errors[:base] << "You already attend to this event" if Attendance.exists?(:user_id => self.user_id, :event_id => self.event_id)
+    end
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,11 @@ class Relationship < ActiveRecord::Base
   belongs_to :user
   belongs_to :related_user, :class_name => 'User'
   
+  validate :validate_unique_by_user_id_and_related_user_id
+  
+  private
+  
+    def validate_unique_by_user_id_and_related_user_id
+      self.errors[:base] << "You already have this user as a friend" if Relationship.exists?(:user_id => self.user_id, :related_user_id => self.related_user_id)
+    end
 end
