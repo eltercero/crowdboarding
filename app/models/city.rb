b6bd76b5329feee4e13208749f00d9b1ab0b0934
Michael Koper <michaelkoper@gmail.com>
2011-05-17 20:57:59 +0200

    downcase city name before create

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,10 @@
 class City < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 100 }
+  
+  before_create :downcase_name
+  
+  private
+    def downcase_name
+      self.name.downcase!
+    end
 end
