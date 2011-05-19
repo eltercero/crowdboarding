ea9ca8ca536b36a795160ba05b107d9b3f9d6959
Michael Koper <michaelkoper@gmail.com>
2011-05-19 04:02:30 +0200

    changed schema and model countries. Inserted better country names in csv

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,25 @@
 class Country < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 100 }
   validates :country_code, :presence => true, :uniqueness => true, :length => { :is => 2 }
-  validates :country_code_big, :presence => true, :uniqueness => true, :length => { :is => 3 }
 
   has_many :cities
+  
+  before_create :downcase_name, :titleize_name, :downcase_country_code
+  
+  def name_without_comma
+    name.gsub(",","")
+  end
+  
+  private    
+    def downcase_name
+      self.name.downcase!
+    end
+    
+    def titleize_name
+      self.name = self.name.titleize
+    end
+    
+    def downcase_country_code
+      self.country_code.downcase!
+    end
 end
