2d2b6c35e8f390adf02da65278caaaea6fb24ec4
Michael Koper <michaelkoper@gmail.com>
2011-05-22 03:10:09 +0200

    added notifier for the flash messages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 	mouseOverHelmet();
+	showNotifyBar();
 });
 
 mouseOverHelmet = function(){
 	$("div#helmet").mouseover(function() {
 		$(this).css('background-position', '0 -123px');
-	}).mouseout(function() {
+	}).mouseout(function() { 
 		$(this).css('background-position', '0 0');
 	});
+}
+
+showNotifyBar = function(){
+	notifier = $('div#wrapper_noticebar');
+	if(notifier.length) {
+		notifier.slideDown().delay(3000).slideUp();
+	}
 }
