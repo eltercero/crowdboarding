045b9c09734577056278076311071d91ccfa71bc
michaelkoper <michaelkoper@gmail.com>
2011-05-25 13:17:34 +0200

    added google in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'vrScGb9FJH9tk57n9Xtag', 'lQCz1gyIKwtntRqgqOiEx4vJEcls5tWrN11kIvvj0'
-  provider :google, '279773890472.apps.googleusercontent.com', 'zT2UUk3S5t1LgdTLX_4TIFtk'
   provider :facebook, '224901847535829', 'a992b5a9f55f854fd0d012ca8d2c39d9'
+  
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 end
