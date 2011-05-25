c7ab8ba2d47863d87f543974d03a00ac71368b31
michaelkoper <michaelkoper@gmail.com>
2011-05-25 13:19:53 +0200

    added filestore require in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # you need a store for OpenID; (if you deploy on heroku you need Filesystem.new('./tmp') instead of Filesystem.new('/tmp'))
+  require 'openid/store/filesystem'
+  
   provider :twitter, 'vrScGb9FJH9tk57n9Xtag', 'lQCz1gyIKwtntRqgqOiEx4vJEcls5tWrN11kIvvj0'
   provider :facebook, '224901847535829', 'a992b5a9f55f854fd0d012ca8d2c39d9'
   
