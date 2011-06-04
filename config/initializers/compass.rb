bea8827c86d45fc0480626abecbb4d2e3a048932
Michael Koper <michaelkoper@gmail.com>
2011-06-04 17:16:09 +0200

    Dont initialize compass in initializer. That is deprecated.

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -4,7 +4,7 @@ require 'compass/app_integration/rails'
 require 'fileutils'
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-Compass::AppIntegration::Rails.initialize!
+# Compass::AppIntegration::Rails.initialize!
 
 Rails.configuration.middleware.delete('Sass::Plugin::Rack')
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
